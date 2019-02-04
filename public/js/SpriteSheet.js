19bcb63d4a5d3b52939b66063d3360efc6dcfa86
BusyReadingSomething <oro1919@gmail.com>
2019-02-03 22:41:45 -0800

    Fixed ESLINT

--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -1,4 +1,13 @@
+/**
+ * Class to contain the sprite tiles and referenve the screen size
+ */
 export default class SpriteSheet {
+  /**
+   * Sets the state of the class
+   * @param {Image} image Tile image
+   * @param {Number} width Screen width
+   * @param {Number} height Screen height
+   */
   constructor(image, width, height) {
     this.image = image;
     this.width = width;
@@ -6,29 +15,49 @@ export default class SpriteSheet {
     this.tiles = new Map();
   }
 
+  /**
+   * Creates the tile from png
+   * @param {String} name Name of the tile
+   * @param {Number} x X position on png
+   * @param {Number} y Y position on png
+   */
   define(name, x, y) {
     const buffer = document.createElement('canvas');
     buffer.width = this.width;
     buffer.height = this.height;
     buffer.getContext('2d')
-      .drawImage(
-        this.image,
-        x * this.width,
-        y * this.height,
-        this.width,
-        this.height,
-        0,
-        0,
-        this.width,
-        this.height);
+        .drawImage(
+            this.image,
+            x * this.width,
+            y * this.height,
+            this.width,
+            this.height,
+            0,
+            0,
+            this.width,
+            this.height);
     this.tiles.set(name, buffer);
   }
 
+  /**
+   * Draws the tile on the canvas.
+   * @param {String} name Name of the tile
+   * @param {Context} context Context of the canvas
+   * @param {Number} x Position on the screen x
+   * @param {Number} y Position on the screen y
+   */
   draw(name, context, x, y) {
     const buffer = this.tiles.get(name);
     context.drawImage(buffer, x, y);
   }
 
+  /**
+   * Calls {@link SpriteSheet.draw} with the proper position.
+   * @param {String} name Name of the tile
+   * @param {Context} context Context of the canvas
+   * @param {Number} x Position to draw on canvas X
+   * @param {Number} y Position to draw on canvas Y
+   */
   drawTile(name, context, x, y) {
     this.draw(name, context, x * this.width, y * this.height);
   }
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,15 @@
-import { resolve as _resolve } from "path";
+import {resolve as _resolve} from 'path';
 import SpriteSheet from './SpriteSheet';
-import { loadImage, loadLevel } from './loaders';
+import {loadImage, loadLevel} from './loaders';
 
+/**
+ * Draws the background based on the given level background.
+ * @param {Object} background information about the background
+ * @param {Context} context Canvas context
+ * @param {SpriteSheet} sprites Contains the sheet of sprites
+ */
 function drawBackground(background, context, sprites) {
-  background.ranges.forEach(([ x1, x2, y1, y2]) => {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
     for (let x = x1; x < x2; x += 1) {
       for (let y = y1; y < y2; y += 1) {
         sprites.drawTile(background.tile, context, x, y);
@@ -16,19 +22,20 @@ const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 loadImage(_resolve(__dirname, './img/tiles.png'))
-  .then(image => {
-    const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define('ground', 0, 0);
-    sprites.define('sky', 3, 23);
+    .then((image) => {
+      const sprites = new SpriteSheet(image, 16, 16);
+      sprites.define('ground', 0, 0);
+      sprites.define('sky', 3, 23);
 
-    loadLevel('1-1')
-      .then(level => {
-        level.backgrounds.forEach(background => drawBackground(background, context, sprites));
-      });
+      loadLevel('1-1')
+          .then((level) => {
+            level.backgrounds.forEach(
+                (background) => drawBackground(background, context, sprites));
+          });
 
-    for (let x = 0; x < 25; x += 1) {
-      for (let y = 12; y < 14; y += 1) {
-        sprites.drawTile('ground', context, x, y);
+      for (let x = 0; x < 25; x += 1) {
+        for (let y = 12; y < 14; y += 1) {
+          sprites.drawTile('ground', context, x, y);
+        }
       }
-    }
-  });
+    });
