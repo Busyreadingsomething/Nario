9353dd9bb6d8ee5ba7431d353f02291fa0387631
BusyReadingSomething <oro1919@gmail.com>
2019-02-03 22:29:08 -0800

    Added basic background drawer with SpriteSheet class and loaders

---
+++ public/js/loaders.js
@@ -0,0 +1,21 @@
+/**
+ * Loads the image of the on the load event
+ * @param {string} url file path of the image
+ * @returns promise of the image being loaded and event listener being added
+ */
+export function loadImage(url) {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.addEventListener('load', () => resolve(image));
+    image.src = url;
+  });
+}
+/**
+ * Loads the level.
+ * @param {string} name the name of the level to be loaded
+ * @returns Json file of the level
+ */
+export function loadLevel(name) {
+  return fetch(`/levels/${name}.json`)
+    .then(r => r.json());
+}
