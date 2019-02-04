97ce235731a1d8ee3f7d5e36a4e1410caf50dc46
BusyReadingSomething <oro1919@gmail.com>
2019-02-03 23:41:47 -0800

    Added layers file to create layers with updated methods

---
+++ public/js/layers.js
@@ -0,0 +1,34 @@
+/**
+ * Draws the background based on the given level background.
+ * @param {Object} background information about the background
+ * @param {Context} context Canvas context
+ * @param {SpriteSheet} sprites Contains the sheet of sprites
+ */
+function drawBackground(background, context, sprites) {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
+    for (let x = x1; x < x2; x += 1) {
+      for (let y = y1; y < y2; y += 1) {
+        sprites.drawTile(background.tile, context, x, y);
+      }
+    }
+  });
+}
+
+/**
+ * Creates the layer that will be drawn when called.
+ * @param {Level} backgrounds Contains the level backgrounds
+ * @param {SpriteSheet} sprites Sprites to be passed into each background
+ * @return {drawBackgroundLayer} Function to be called to draw the later
+ */
+export function createBackgroundLayer(backgrounds, sprites) {
+  const buffer = document.createElement('canvas');
+  buffer.width = 256;
+  buffer.height = 240;
+
+  backgrounds.forEach((background) =>
+    drawBackground(background, buffer.getContext('2d'), sprites));
+
+  return function drawBackgroundLayer(context) {
+    context.drawImage(buffer, 0, 0);
+  };
+}
