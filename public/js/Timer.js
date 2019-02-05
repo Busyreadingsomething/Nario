d30e981e900affea73a0ed1626b29452a598dd1e
BusyReadingSomething <oro1919@gmail.com>
2019-02-04 22:37:11 -0800

    Added Timer class for animation

---
+++ public/js/Timer.js
@@ -0,0 +1,38 @@
+/** Sets the timiing for the game */
+export default class Timer {
+  /**
+   * Sets the framerate for the timer.
+   * @param {Number} deltaTime framerate default at 1/60
+   */
+  constructor(deltaTime = 1/60) {
+    let accumulatedTime = 0;
+    let oldTime = 0;
+    /**
+     * Updates Mario's position based on the time and velocity through
+     * {@link requestAnimationFrame}. The delta time sets the framerate,
+     * and the accumulated time makes sure that the speed of the calculation
+     * doesn't mess with the animation.
+     * @param {Number} time milliseconds of the call
+     */
+    this.updateProxy = (time) => {
+      accumulatedTime += (time - oldTime) / 1000;
+
+      while (accumulatedTime > deltaTime) {
+        this.update(deltaTime);
+        accumulatedTime -= deltaTime;
+      }
+      oldTime = time;
+
+      this.enqueue();
+    };
+  }
+  /** Enqueues the next update */
+  enqueue() {
+    requestAnimationFrame(this.updateProxy);
+  }
+
+  /** Starts the timer. */
+  start() {
+    this.enqueue();
+  }
+}
