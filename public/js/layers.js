0fef86b805c44d0ff8212acd8c460b5725aa0e4c
BusyReadingSomething <oro1919@gmail.com>
2019-02-05 23:31:55 -0800

    Added Tile collision layer method, and updated load level to completed draw the level and return the class.

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,32 +1,19 @@
 /**
- * Draws the background based on the given level background.
- * @param {Object} background information about the background
- * @param {Context} context Canvas context
- * @param {SpriteSheet} sprites Contains the sheet of sprites
- */
-function drawBackground(background, context, sprites) {
-  background.ranges.forEach(([x1, x2, y1, y2]) => {
-    for (let x = x1; x < x2; x += 1) {
-      for (let y = y1; y < y2; y += 1) {
-        sprites.drawTile(background.tile, context, x, y);
-      }
-    }
-  });
-}
-
-/**
  * Creates the layer that will be drawn when called.
- * @param {Level} backgrounds Contains the level backgrounds
+ * @param {Level} level Level object
  * @param {SpriteSheet} sprites Sprites to be passed into each background
  * @return {drawBackgroundLayer} Function to be called to draw the later
  */
-export function createBackgroundLayer(backgrounds, sprites) {
+export function createBackgroundLayer(level, sprites) {
   const buffer = document.createElement('canvas');
   buffer.width = 256;
   buffer.height = 240;
 
-  backgrounds.forEach((background) =>
-    drawBackground(background, buffer.getContext('2d'), sprites));
+  const context = buffer.getContext('2d');
+
+  level.tiles.forEach((tile, x, y) => {
+    sprites.drawTile(tile.name, context, x, y);
+  });
 
   return function drawBackgroundLayer(context) {
     context.drawImage(buffer, 0, 0);
@@ -35,11 +22,52 @@ export function createBackgroundLayer(backgrounds, sprites) {
 
 /**
  * Creates a sprite layer to be drawn later
- * @param {Entity} entity Sprite information
+ * @param {Set<Entity>} entities Sprite information
  * @return {drawSpriteLayer} function to draw sprite when called
  */
-export function createSpriteLayer(entity) {
+export function createSpriteLayer(entities) {
   return function drawSpriteLayer(context) {
-    entity.draw(context);
+    entities.forEach((entity) => entity.draw(context));
+  };
+}
+
+/**
+ * Creates the collision layer.
+ * @param {Level} level Current level of the game.
+ * @return {Function} drawCollision function;
+ */
+export function createCollisionLayer(level) {
+  const resolvedTiles = [];
+  const tileResolver = level.tileCollider.tiles;
+  const tileSize = tileResolver.tileSize;
+
+  const getByIndexOriginal = tileResolver.getByIndex;
+  tileResolver.getByIndex = function getByIndexFake(x, y) {
+    resolvedTiles.push({x, y});
+    return getByIndexOriginal.call(tileResolver, x, y);
+  };
+
+  return function drawCollision(context) {
+    context.strokeStyle = 'blue';
+    resolvedTiles.forEach(({x, y}) => {
+      context.beginPath();
+      context.rect(
+          x * tileSize,
+          y * tileSize,
+          tileSize,
+          tileSize);
+      context.stroke();
+    });
+
+    context.strokeStyle = 'red';
+    level.entities.forEach((entity) => {
+      context.beginPath();
+      context.rect(
+          entity.pos.x, entity.pos.y,
+          entity.size.x, entity.size.y);
+      context.stroke();
+    });
+
+    resolvedTiles.length = 0;
   };
 }
--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,3 +1,7 @@
+import Level from './Level';
+import {createBackgroundLayer, createSpriteLayer} from './layers';
+import {loadBackgroundSprites} from './sprites';
+
 /**
  * Loads the image of the on the load event
  * @param {string} url file path of the image
@@ -10,12 +14,47 @@ export function loadImage(url) {
     image.src = url;
   });
 }
+
+/**
+ * Adds the tiles to the level.
+ * @param {Level} level level of the game
+ * @param {Array} backgrounds List of backgrounds
+ */
+function createTiles(level, backgrounds) {
+  backgrounds.forEach((background) => {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+      for (let x = x1; x < x2; x += 1) {
+        for (let y = y1; y < y2; y += 1) {
+          level.tiles.set(x, y, {
+            name: background.tile,
+          });
+        }
+      }
+    });
+  });
+};
+
 /**
  * Loads the level.
  * @param {string} name the name of the level to be loaded
  * @return {Promise<Object>}Json file of the level
  */
 export function loadLevel(name) {
-  return fetch(`/levels/${name}.json`)
-      .then((r) => r.json());
+  return Promise.all([
+    fetch(`/levels/${name}.json`)
+        .then((r) => r.json()),
+    loadBackgroundSprites(),
+  ]).then(([levelSpec, backgroundSprites]) => {
+    const level = new Level();
+
+    createTiles(level, levelSpec.backgrounds);
+
+    const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
+    level.comp.layers.push(backgroundLayer);
+
+    const spriteLayer = createSpriteLayer(level.entities);
+    level.comp.layers.push(spriteLayer);
+
+    return level;
+  });
 }
