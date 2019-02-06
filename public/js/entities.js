536a359becb054a2df3451c78b0d43af6b358695
BusyReadingSomething <oro1919@gmail.com>
2019-02-05 23:33:48 -0800

    Added sit and reoardered traits.

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,6 +1,7 @@
 import Entity from './Entity';
-import Velocity from './traits/Velocity';
+import Go from './traits/Go';
 import Jump from './traits/Jump';
+// import Velocity from './traits/Velocity';
 import {loadMarioSprite} from './sprites';
 /**
  * Creates the mario entity with the draw and update methods.
@@ -9,9 +10,11 @@ import {loadMarioSprite} from './sprites';
 export function createMario() {
   return loadMarioSprite().then((sprite) => {
     const mario = new Entity();
+    mario.size.set(14, 16);
 
-    mario.addTrait(new Velocity());
+    mario.addTrait(new Go());
     mario.addTrait(new Jump());
+    // mario.addTrait(new Velocity());
 
     mario.draw = function drawMario(context) {
       sprite.draw('idle', context, this.pos.x, this.pos.y);
