36823016099f9e1c19bfcb39c09797dd67cc624b
BusyReadingSomething <oro1919@gmail.com>
2019-02-06 23:56:59 -0800

    Added loadSpriteSheet, and loadJSON to process the sprites. and updated createTiles to use updated methods. loadLevel now creates the tiles.

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,6 +1,6 @@
 import Level from './Level';
+import SpriteSheet from './SpriteSheet';
 import {createBackgroundLayer, createSpriteLayer} from './layers';
-import {loadBackgroundSprites} from './sprites';
 
 /**
  * Loads the image of the on the load event
@@ -16,45 +16,99 @@ export function loadImage(url) {
 }
 
 /**
+ * Loads the JSON.
+ * @param {String} url locaation of JSON
+ * @return {JSON} JSON file.
+ */
+function loadJSON(url) {
+  return fetch(url).then((r) => r.json());
+}
+
+/**
  * Adds the tiles to the level.
  * @param {Level} level level of the game
  * @param {Array} backgrounds List of backgrounds
  */
 function createTiles(level, backgrounds) {
+  /**
+   * Parses the range.
+   * @param {Object} background Data with the background info.
+   * @param {Number} xStart Start of X
+   * @param {Number} xLen Length of X
+   * @param {Number} yStart Start of Y
+   * @param {Number} yLen Length of Y
+   */
+  function applyRange(background, xStart, xLen, yStart, yLen) {
+    const xEnd = xStart + xLen;
+    const yEnd = yStart + yLen;
+    for (let x = xStart; x < xEnd; x += 1) {
+      for (let y = yStart; y < yEnd; y += 1) {
+        level.tiles.set(x, y, {
+          name: background.tile,
+          type: background.type,
+        });
+      }
+    }
+  }
   backgrounds.forEach((background) => {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-      for (let x = x1; x < x2; x += 1) {
-        for (let y = y1; y < y2; y += 1) {
-          level.tiles.set(x, y, {
-            name: background.tile,
-          });
-        }
+    background.ranges.forEach((range) => {
+      if (range.length === 4) {
+        applyRange(background, ...range);
+      } else if (range.length === 3) {
+        const [xStart, xLen, yStart] = range;
+        applyRange(background, xStart, xLen, yStart, 1);
+      } else if (range.length === 2) {
+        const [xStart, yStart] = range;
+        applyRange(background, xStart, 1, yStart, 1);
       }
     });
   });
 };
 
 /**
+ * Loads the sprite sheet.
+ * @param {String} name of sheet.
+ * @return {JSON} Sheet information.
+ */
+function loadSpriteSheet(name) {
+  return loadJSON(`/sprites/${name}.json`)
+      .then((sheetSpec) => Promise.all([
+        sheetSpec,
+        loadImage(sheetSpec.imageURL),
+      ]))
+      .then(([sheetSpec, image]) => {
+        const sprites = new SpriteSheet(
+            image, sheetSpec.tileW, sheetSpec.tileH);
+
+        sheetSpec.tiles.forEach((tileSpec) => {
+          sprites.defineTile(
+              tileSpec.name, tileSpec.index[0], tileSpec.index[1]);
+        });
+        return sprites;
+      });
+}
+
+/**
  * Loads the level.
- * @param {string} name the name of the level to be loaded
- * @return {Promise<Object>}Json file of the level
+ * @param {String} name the name of the level to be loaded
+ * @return {Promise<JSON>}Json file of the level
  */
 export function loadLevel(name) {
-  return Promise.all([
-    fetch(`/levels/${name}.json`)
-        .then((r) => r.json()),
-    loadBackgroundSprites(),
-  ]).then(([levelSpec, backgroundSprites]) => {
-    const level = new Level();
+  return loadJSON(`/levels/${name}.json`)
+      .then((levelSpec) => Promise.all([
+        levelSpec,
+        loadSpriteSheet(levelSpec.spriteSheet),
+      ])).then(([levelSpec, backgroundSprites]) => {
+        const level = new Level();
 
-    createTiles(level, levelSpec.backgrounds);
+        createTiles(level, levelSpec.backgrounds);
 
-    const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
-    level.comp.layers.push(backgroundLayer);
+        const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
+        level.comp.layers.push(backgroundLayer);
 
-    const spriteLayer = createSpriteLayer(level.entities);
-    level.comp.layers.push(spriteLayer);
+        const spriteLayer = createSpriteLayer(level.entities);
+        level.comp.layers.push(spriteLayer);
 
-    return level;
-  });
+        return level;
+      });
 }
