970651a0e0b6a4c85dd6ef7b866af5ccda8c0701
BusyReadingSomething <oro1919@gmail.com>
2019-02-09 13:48:52 -0800

    Create run animation for Mario.

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,23 +1,35 @@
 import Entity from './Entity';
 import Go from './traits/Go';
 import Jump from './traits/Jump';
-// import Velocity from './traits/Velocity';
-import {loadMarioSprite} from './sprites';
+import {loadSpriteSheet} from './loaders';
+import {createAnim} from './anim';
 /**
  * Creates the mario entity with the draw and update methods.
  * @return {Promise<Entity>} mario entity
  */
 export function createMario() {
-  return loadMarioSprite().then((sprite) => {
+  return loadSpriteSheet('mario').then((sprite) => {
     const mario = new Entity();
     mario.size.set(14, 16);
 
     mario.addTrait(new Go());
     mario.addTrait(new Jump());
-    // mario.addTrait(new Velocity());
+
+    const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 10);
+    /**
+     * Determines the frame for mario.
+     * @param {Entity} mario Mario's sprite entity
+     * @return {String} frame type
+     */
+    function routeFrame(mario) {
+      if (mario.go.dir !== 0) {
+        return runAnim(mario.go.distance);
+      }
+      return 'idle';
+    }
 
     mario.draw = function drawMario(context) {
-      sprite.draw('idle', context, 0, 0);
+      sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
     };
 
     return mario;
