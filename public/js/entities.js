7e5aff6d709404b29818a1684a8850d6296183eb
BusyReadingSomething <oro1919@gmail.com>
2019-02-09 19:16:23 -0800

    Added jumping animation and adjusted drag rates on B pressed.

--- public/js/entities.js
+++ public/js/entities.js
@@ -3,6 +3,10 @@ import Go from './traits/Go';
 import Jump from './traits/Jump';
 import {loadSpriteSheet} from './loaders';
 import {createAnim} from './anim';
+
+const SLOW_DRAG = 1/2000;
+const FAST_DRAG = 1/5000;
+
 /**
  * Creates the mario entity with the draw and update methods.
  * @return {Promise<Entity>} mario entity
@@ -15,14 +19,28 @@ export function createMario() {
     mario.addTrait(new Go());
     mario.addTrait(new Jump());
 
-    const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 10);
+    mario.go.dragFactor = SLOW_DRAG;
+
+    mario.turbo = function setTurboState(turboOn) {
+      this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
+    };
+
+    const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6);
     /**
      * Determines the frame for mario.
      * @param {Entity} mario Mario's sprite entity
      * @return {String} frame type
      */
     function routeFrame(mario) {
-      if (mario.go.dir !== 0) {
+      if (mario.jump.falling) {
+        return 'jump';
+      }
+
+      if (mario.go.distance > 0) {
+        if ((mario.vel.x > 0 && mario.go.dir < 0)
+          || (mario.vel.x < 0 && mario.go.dir > 0)) {
+          return 'break';
+        }
         return runAnim(mario.go.distance);
       }
       return 'idle';
