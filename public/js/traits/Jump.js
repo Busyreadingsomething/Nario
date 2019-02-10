a7ed76092865421ce055b36ee1d271ab23c4ce55
BusyReadingSomething <oro1919@gmail.com>
2019-02-09 19:18:01 -0800

    Added obstruction logic to and grace period for jump inputs.

--- public/js/traits/Jump.js
+++ public/js/traits/Jump.js
@@ -1,4 +1,4 @@
-import {Trait} from '../Entity';
+import {Trait, Sides} from '../Entity';
 
 /** Jump class for entities. */
 export default class Jump extends Trait {
@@ -6,14 +6,23 @@ export default class Jump extends Trait {
   constructor() {
     super('jump');
 
-    this.duration = 0.5;
+    this.ready = 0;
+    this.duration = 0.3;
     this.velocity = 200;
     this.engageTime = 0;
+    this.requestTime = 0;
+    this.gracePeriod = 0.1;
+    this.speedBoost = 0.3;
+  }
+
+  /** Calls if falling or not. */
+  get falling() {
+    return this.ready < 0;
   }
 
   /** Starts the jump. */
   start() {
-    this.engageTime = this.duration;
+    this.requestTime = this.gracePeriod;
   }
 
   /** Cancels the jump. */
@@ -22,14 +31,39 @@ export default class Jump extends Trait {
   }
 
   /**
+   * Sets the read to true if obstructed by bottom.
+   * @param {Entity} entity Sprite entity.
+   * @param {String} side side of obstruction.
+   */
+  obstruct(entity, side) {
+    if (side === Sides.BOTTOM) {
+      this.ready = 1;
+    } else if (side === Sides.TOP) {
+      this.cancel();
+    }
+  }
+
+  /**
    * Updates the entity position based on time and velocity.
+   * Sets ready to false.
    * @param {Entity} entity Sprite entity
    * @param {Number} deltaTime Change in time
    */
   update(entity, deltaTime) {
+    if (this.requestTime > 0) {
+      if (this.ready > 0) {
+        this.engageTime = this.duration;
+        this.requestTime = 0;
+      }
+
+      this.requestTime -= deltaTime;
+    }
     if (this.engageTime > 0) {
-      entity.vel.y = -this.velocity;
+      entity.vel.y =
+        -(this.velocity + Math.abs(entity.vel.x) * this.speedBoost);
       this.engageTime -= deltaTime;
     }
+
+    this.ready -= 1;
   }
 }
