483350359a7209d96853992c3ba9d75a95c68977
BusyReadingSomething <oro1919@gmail.com>
2019-02-20 21:07:03 -0800

    Added BoundingBox class to keep track of the bounds, and called appropriately from entity.

---
+++ public/js/BoundingBox.js
@@ -0,0 +1,65 @@
+/**  */
+export default class BoundingBox {
+  /**
+   * @param {Vec2} pos Position of sprite.
+   * @param {Vec2} size Size of sprite.
+   * @param {Vec2} offset Sprite offset.
+   */
+  constructor(pos, size, offset) {
+    this.pos = pos;
+    this.size = size;
+    this.offset = offset;
+  }
+
+  /** Gets bottom position. */
+  get bottom() {
+    return this.pos.y + this.size.y + this.offset.y;
+  }
+
+  /** Gets top position. */
+  get top() {
+    return this.pos.y + this.offset.y;
+  }
+
+  /** Gets left position. */
+  get left() {
+    return this.pos.x + this.offset.x;
+  }
+
+  /** Gets right position. */
+  get right() {
+    return this.pos.x + this.size.x + this.offset.x;
+  }
+
+  /**
+   * Sets bottom position.
+   * @param {Number} y
+   */
+  set bottom(y) {
+    this.pos.y = y - (this.size.y + this.offset.y);
+  }
+
+  /**
+   * Sets top position.
+   * @param {Number} y
+   */
+  set top(y) {
+    this.pos.y = y - this.offset.y;
+  }
+
+  /**
+   * Sets left position.
+   * @param {Number} x
+   */
+  set left(x) {
+    this.pos.x = x - this.offset.x;
+  }
+
+  /**
+   * Sets right position.
+   * @param {Number} x
+   */
+  set right(x) {
+    return this.pos.x = x - (this.size.x + this.offset.x);
+  }
+}
