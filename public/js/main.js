bfd3f5aad420f9a82938df7d1f823c8782a6bebd
BusyReadingSomething <oro1919@gmail.com>
2019-02-20 21:10:36 -0800

    Imported loadEntities method, and created Goomba and Koopa entities in game.

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,33 @@
 import Timer from './Timer';
 import Camera from './Camera';
 import {loadLevel} from './loaders/level';
-import {createMario} from './entities';
+import {loadEntities} from './entities';
 import {setUpKeyBoard} from './input';
+import {createCollisionLayer} from './layers';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
-  createMario(),
+  loadEntities(),
   loadLevel('1-1'),
-]).then(([mario, level]) => {
+]).then(([entity, level]) => {
   const camera = new Camera();
 
+  const mario = entity.mario();
   mario.pos.set(64, 64);
 
+  const goomba = entity.goomba();
+  goomba.pos.x = 220;
+  level.entities.add(goomba);
+
+  const koopa = entity.koopa();
+  koopa.pos.x = 260;
+  level.entities.add(koopa);
+
   level.entities.add(mario);
+
+  level.comp.layers.push(createCollisionLayer(level));
 
   const input = setUpKeyBoard(mario);
   input.listenTo(window);
